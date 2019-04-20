23f841563b0ebfe690542ba190094e731339ad02
kmead <kimmeadows8@gmail.com>
2019-04-19 21:20:22 -0500

    add migrations

---
+++ db/migrate/20190420021506_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[5.2]
+  def change
+    create_table :trips do |t|
+      t.integer :user_id 
+      t.integer :city_id 
+      t.integer :rating 
+      t.text :comment  
+
+      t.timestamps null:false 
+    end
+  end
+end
