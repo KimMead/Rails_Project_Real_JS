7de59130c5f0a4cefadffed0017cdd6f67c9274a
kmead <kimmeadows8@gmail.com>
2019-04-21 10:16:10 -0500

    change city to attraction in db

--- db/migrate/20190421151151_create_attractions.rb
+++ db/migrate/20190421151151_create_attractions.rb
@@ -1,8 +1,9 @@
-class CreateCities < ActiveRecord::Migration[5.2]
+class CreateAttractions < ActiveRecord::Migration[5.2]
   def change
-    create_table :cities do |t|
+    create_table :attractions do |t|
       t.string :name 
       t.integer :state_id 
+
 
       t.timestamps null: false 
     end
