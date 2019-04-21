9bfeb4c2d8479de6d1e4c1ffa4a10064f3c1b91a
kmead <kimmeadows8@gmail.com>
2019-04-21 17:13:12 -0500

    add sessions controller and users controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController 
+    before_action :require_login
+
+    def create 
+        @user = User.find_by(name: params[:name])
+        if @user && authenticate(params[:password]) 
+            session[:user_id] = @user.id 
+            
+            redirect_to user_path(@user)
+        else 
+            redirect_to signin_path 
+        end 
+    end 
+    
+    def destroy 
+        session.delete :user_id
+        redirect_to root_path
+    end 
+    
+    private
+     
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+        if !logged_in?
+            flash[:error] = "You must be logged in to view this page."
+            redirect_to login_path
+        end
+    end
+end 
