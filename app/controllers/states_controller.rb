5fe87db14434bb8c9d4a5d69d6e3901f67734c9a
kmead <kimmeadows8@gmail.com>
2019-04-25 13:41:45 -0500

    add state show path

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,20 +1,23 @@
 class StatesController < ApplicationController
     
     def index
-        @states = States.all 
+        @states = State.all 
     end 
 
     def new
-        
+        @state = State.new
     end
-
+    
     def create
-        
-    end
+        @state = State.new(state_params)
+        if @state.save
+          redirect_to show_path(@state)
+        else
+          render :new
+        end
+      end
 
     def show
-       
+        @state = State.find_by(id: params[:id])
     end 
-
-    
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ root 'welcome#home'
 resources :users, except: [:index]
 resources :trips
 resources :attractions, only: [:new, :create, :show]
-resources :cities
+resources :states, only: [:index, :show]
 
 get '/signup', to: 'users#new'
 post '/signup', to: 'users#create' 
@@ -15,5 +15,5 @@ post '/signin', to: 'sessions#create'
 
 get '/logout', to: 'sessions#destroy'
 
-
+post '/states', to: 'states#show'
 end
