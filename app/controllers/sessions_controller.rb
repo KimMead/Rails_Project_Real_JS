b7611e603f4a8d528e92464c2d53bf517655f0f3
kmead <kimmeadows8@gmail.com>
2019-05-12 12:28:18 -0500

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       redirect_to signin_path
     end
