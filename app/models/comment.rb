ee4d166aa96decc2eaa5f9108533e4ea485ed4ec
kmead <kimmeadows8@gmail.com>
2019-05-26 14:13:12 -0500

    add flash message for signin error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,4 @@ class Comment < ApplicationRecord
     belongs_to :state 
     belongs_to :user 
 
-    def self.most_comments
-        joins(:comments).group(:id).order('count(comments.id) DESC').first
-      end
-
 end
