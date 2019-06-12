098345191c3a873fd3acec0602a5156e9e706d04
kmead <kimmeadows8@gmail.com>
2019-06-11 21:45:34 -0500

    Add github fields to user table

---
+++ db/migrate/20190612023950_add_avatar_url_to_users.rb
@@ -0,0 +1,4 @@
+class AddAvatarUrlToUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_173915) do
+ActiveRecord::Schema.define(version: 2019_06_12_023950) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
